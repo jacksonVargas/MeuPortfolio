5a911badb6afdd5d743050e1a06c8a21cb84a626
jackson <jacksonvargas134@gmail.com>
2025-11-07 12:41:04 -0300

    teste

--- src/constants.tsx
+++ src/constants.tsx
@@ -12,7 +12,7 @@ export const projects: Project[] = [
   {
     id: 1,
     title: 'Clínica Odontológica',
-    description: 'Uma landing page profissional feita para uma clínica odontológica, com um design moderno e reponsivo.',
+    description: 'Uma landing page profissional feita para uma clínica odontológica, com um design moderno e responsivo.',
     image: 'https://s10.aconvert.com/convert/p3r68-cdx67/ael75-0v054.jpg',
     tags: ['Next', 'Tailwind'],
     gitUrl: 'https://github.com/jacksonVargas/landingPageClinica',
