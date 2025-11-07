20970e0307c5f3e8f651d08b1c7a93f6604b6285
jackson <jacksonvargas134@gmail.com>
2025-11-07 15:53:46 -0300

    imagens

--- src/constants.tsx
+++ src/constants.tsx
@@ -13,9 +13,11 @@ export const projects: Project[] = [
     id: 1,
     title: 'Clínica Odontológica',
     description: 'Uma landing page profissional feita para uma clínica odontológica, com um design moderno e responsivo.',
-    image: 'https://s10.aconvert.com/convert/p3r68-cdx67/a2gl3-7z22l.webp',
+    image: '../public/imageClinica.png',
     tags: ['Next', 'Tailwind'],
     gitUrl: 'https://github.com/jacksonVargas/landingPageClinica',
     demoUrl: 'https://landing-page-clinica-kohl.vercel.app/',
   }
 ]
+
+// https://s10.aconvert.com/convert/p3r68-cdx67/a2gl3-7z22l.webp
